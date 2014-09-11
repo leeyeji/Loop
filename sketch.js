14a46bb7da22080170480df722b2d4d1b50013d6
leeyeji <miracle458393@gmail.com>
2014-09-11 15:37:15 +0900

    add 5
    5

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(640, 480);
+  background(128, 0, 64);
+  noFill();
+
+  stroke(255, 0, 0);
+
+  for (var x = 1; x <=8; x++)
+{
+  ellipse(x * 70, x * 20 + 100, 50, 50);
+}
+}
