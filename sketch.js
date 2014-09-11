c3a08ba7e3ec4f51f29e8c986c77b03ae1f51d1c
leeyeji <miracle458393@gmail.com>
2014-09-11 16:02:57 +0900

    add 7
    7

--- sketch.js
+++ sketch.js
@@ -3,10 +3,10 @@ function setup() {
   background(128, 0, 64);
   noFill();
 
-  stroke(255, 0, 0);
+}
+function draw() {
+  background(128, 0, 64);
 
-  for (var x = 1; x <=8; x++)
-{
-  ellipse(x * 70, x * 20 + 100, x* 30, x* 10);
+  ellipse(320, 240, mouseX, mouseY);
 }
-}
+
